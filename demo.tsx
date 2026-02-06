15555a80f8da9da7cc67367402daaaa29acafab7
Thomas Lindet <t.lindet@parnasse-education.fr>
2026-02-06 18:44:06 +0100

    update package configuration and add TSX support with new injection syntax

---
+++ demo.tsx
@@ -0,0 +1,28 @@
+import type { FC } from 'hono/jsx'
+
+import { html } from 'hono/html'
+
+const ContratTab: FC = ({ data, existing }) => {
+  return (
+    <>
+      {html`<script type="text/hyperscript">
+        def updateContratBadge()
+          repeat in <[data-tab="contrat"]/>
+            if it's value then
+              increment filled
+            end
+            increment total
+            end
+            put filled+"/"+total into #contrat-badge
+            if filled === total then
+              add .bg-success to #contrat-badge
+              remove .bg-danger from #contrat-badge
+            else
+              add .bg-danger to #contrat-badge
+              remove .bg-success from #contrat-badge
+            end
+      </script>`}
+    </>
+  )
+}
+export default ContratTab
